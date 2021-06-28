ca4c3ad72386d83be24b2e6470488cb7996294ec
Sajal Gupta <guptasajal411@gmail.com>
2021-06-28 21:39:39 +0530

    Update card-footer and add JavaScript for clicking on checkboxes

--- index.js
+++ index.js
@@ -26,37 +26,78 @@ $(".question-2").click(function(){
     $("#question-2-no").css("border-bottom-right-radius", "5px");
 })
 
-$(".firstCheckbox").click(function () {
+$(".firstFooter").click(function () {
     if($(".firstCheckbox").is(":checked")){
+        $(".firstCard").css("border", "0");
+        $(".firstCheckbox").prop("checked",false);
+    }
+    else{
         $(".firstCard").css("border", "5px solid #0C63E4");
         $(".secondCard").css("border", "0");
         $(".thirdCard").css("border", "0");
         $(".secondCheckbox").prop("checked",false);
         $(".thirdCheckbox").prop("checked",false);
         console.log("checked")
-    }
-    else{
-        $(".firstCard").css("border", "0");
-
+        $(".firstCheckbox").prop("checked",true);
     }
 })
-$(".secondCheckbox").click(function () {
+$(".firstCheckbox").click(function () {
+    if($(".firstCheckbox").is(":checked")){
+        $(".firstCard").css("border", "0");
+        $(".firstCheckbox").prop("checked",false);
+    }
+    else{
+        $(".firstCard").css("border", "5px solid #0C63E4");
+        $(".secondCard").css("border", "0");
+        $(".thirdCard").css("border", "0");
+        $(".secondCheckbox").prop("checked",false);
+        $(".thirdCheckbox").prop("checked",false);
+        console.log("checked")
+        $(".firstCheckbox").prop("checked",true);
+    }
+})
+
+$(".secondFooter").click(function () {
     if($(".secondCheckbox").is(":checked")){
+        $(".secondCard").css("border", "0");
+        $(".secondCheckbox").prop("checked",false);
+    }
+    else{
+        $(".secondCheckbox").prop("checked",true);
         $(".secondCard").css("border", "5px solid #0C63E4");
         $(".thirdCard").css("border", "0");
         $(".firstCard").css("border", "0");
         $(".firstCheckbox").prop("checked",false);
         $(".thirdCheckbox").prop("checked",false);
-        console.log("checked")
-    }
-    else{
-        $(".secondCard").css("border", "0");
-
+        $(".secondCheckbox").prop("checked",true);
+        console.log("checked");
     }
 })
-$(".thirdCheckbox").click(function () {
-    
+
+$(".secondCheckbox").click(function () {
+    if($(".secondCheckbox").is(":checked")){
+        $(".secondCard").css("border", "0");
+        $(".secondCheckbox").prop("checked",false);
+    }
+    else{
+        $(".secondCheckbox").prop("checked",true);
+        $(".secondCard").css("border", "5px solid #0C63E4");
+        $(".thirdCard").css("border", "0");
+        $(".firstCard").css("border", "0");
+        $(".firstCheckbox").prop("checked",false);
+        $(".thirdCheckbox").prop("checked",false);
+        console.log("checked");
+        $(".secondCheckbox").prop("checked",true);
+    }
+})
+
+$(".thirdFooter").click(function () {
     if($(".thirdCheckbox").is(":checked")){
+        $(".thirdCard").css("border", "0");
+        $(".thirdCheckbox").prop("checked",false);
+    }
+    else{
+        $(".thirdCheckbox").prop("checked",true);
         $(".thirdCard").css("border", "5px solid #0C63E4");
         $(".firstCard").css("border", "0");
         $(".secondCard").css("border", "0");
@@ -64,8 +105,21 @@ $(".thirdCheckbox").click(function () {
         $(".secondCheckbox").prop("checked",false);
         console.log("checked")
     }
-    else{
+
+})
+$(".thirdCheckbox").click(function () {
+    if($(".thirdCheckbox").is(":checked")){
         $(".thirdCard").css("border", "0");
+        $(".thirdCheckbox").prop("checked",false);
+    }
+    else{
+        $(".thirdCheckbox").prop("checked",true);
+        $(".thirdCard").css("border", "5px solid #0C63E4");
+        $(".firstCard").css("border", "0");
+        $(".secondCard").css("border", "0");
+        $(".firstCheckbox").prop("checked",false);
+        $(".secondCheckbox").prop("checked",false);
+        console.log("checked")
     }
 })
 
